aab9fe09816f060869d61551ba49199a38a42ee5
Analyn Flores <analynflores@gmail.com>
2018-03-08 15:19:45 +0800

    lesson2-event

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,46 @@
-
 import React, { Component } from 'react';
+import { reactDom } from 'react-dom';
 import { render } from 'react-dom';
-import createReactClass from 'create-react-class';
-import { MessageRenderer } from './message';
-import { Labels } from './label';
-import { LabelsComposition } from './labelcomposition';
-import { Counter } from './counter';
-import App from './components/app';
-import { BrowserRouter } from 'react-router-dom'
+
 
 import '../css/style.css';
 
-// Create a component named MessageComponent
-var MessageComponent = createReactClass({
+class EventComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this); 
+  }
+  handleClick(e) {
+    console.log("event.nativeEvent => ");
+    console.log(e.nativeEvent);
+
+    console.log("--------------------------");
+  }
+  handleBlur(e) {
+    console.log("Input value is => " + e.target.value);
+    console.log("--------------------------");
+  }
+  handleClickWithArgs(param, e) {
+    console.log("Param passed is => " + param);
+    console.log("--------------------------");
+  }
   render() {
     return (
-      <div styel="background-color: {this.props.color}">{this.props.message}</div>
+      <div>
+        <h3>Event in React.js Component </h3>
+
+        <button onClick={this.handleClick}>Click me to see the 'event' object in console log!</button>
+        <br/>
+        <br/>
+
+        <input type="text" defaultValue="" onBlur={this.handleBlur} />
+        <br/>
+        <br/>
+
+        <button onClick={this.handleClickWithArgs.bind(this, 'React.js Demo')}>Pass param to event</button>
+      </div>
     );
   }
-});
+}
 
-// Render an instance of MessageComponent into document.body
-//render( <MessageComponent message="Hello!" color="red" />, document.getElementById("app"));
-//render( <MessageRenderer message="Hi!" />, document.getElementById("app"));
-render( <Labels/>, document.getElementById("app"));
-//render( <LabelsComposition/>, document.getElementById("app"));
-//render( <Counter/>, document.getElementById("app"));
-//render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("app"));
+render( <EventComponent/>, document.getElementById("app"));
